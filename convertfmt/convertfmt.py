b9ab7b60c78c19d0799f3ea7e84a6b7da96947bd
zhang dong <dongzhang0725@gmail.com>
2016-07-19 09:33:35 +0800

    Update convertfmt.py

--- convertfmt/convertfmt.py
+++ convertfmt/convertfmt.py
@@ -4,6 +4,7 @@
 __date__  =  '2016年5月18日'
 __author__  =  'zhang dong;708986950@qq.com'
 
+
 class Handle_file:
     def __init__(self,file):
         self.file = file        
@@ -60,25 +61,25 @@ class Handle_file:
                     self.line = self.f.readline()
                 self.assign()    
     def save(self,inputfile):
-        file_path=os.path.dirname(inputfile) if os.path.dirname(inputfile) else '.'
         if myargs.phy:
-            with open(file_path+'/'+os.path.basename(inputfile).split('.')[0]+'.phy','w') as f1:
+            with open(myargs.out+'/'+os.path.basename(inputfile).split('.')[0]+'.phy','w') as f1:
                 f1.write(self.phy)
         if myargs.nex:
-            with open(file_path+'/'+os.path.basename(inputfile).split('.')[0]+'.nex','w') as f2:
+            with open(myargs.out+'/'+os.path.basename(inputfile).split('.')[0]+'.nex','w') as f2:
                 f2.write(self.nex)
         if myargs.paml:
-            with open(file_path+'/'+os.path.basename(inputfile).split('.')[0]+'.PML','w') as f3:
+            with open(myargs.out+'/'+os.path.basename(inputfile).split('.')[0]+'.PML','w') as f3:
                 f3.write(self.paml)
         if myargs.axt:
-            with open(file_path+'/'+os.path.basename(inputfile).split('.')[0]+'.axt','w') as f4:
+            with open(myargs.out+'/'+os.path.basename(inputfile).split('.')[0]+'.axt','w') as f4:
                 f4.write(self.axt)
         if myargs.stat:
-            with open(os.path.dirname(inputfile)+'./statistics.csv','w') as f5:
+            with open(myargs.out+'./statistics.csv','w') as f5:
                 f5.write(self.statistics)       
 if __name__ == '__main__':
-    import re,argparse,sys,os
+    import re,argparse,sys,os,shutil
     def parameter():
+        scripts_path=os.path.dirname(sys.argv[0]) if os.path.dirname(sys.argv[0]) else '.'
         parser = argparse.ArgumentParser(\
                 formatter_class=argparse.RawTextHelpFormatter,\
                 prog = 'convertfmt.py',\
@@ -87,8 +88,9 @@ if __name__ == '__main__':
     examples:
             1.python C:\Users\Administrator\Desktop\scripts\convertfmt.py -f C:\Users\Administrator\Desktop\scripts\demo.fasta -phy
             2.python C:\Users\Administrator\Desktop\scripts\convertfmt.py -f demo.fasta -phy -nex -paml -axt -stat 
+            3.python C:\Users\Administrator\Desktop\scripts\convertfmt.py -f C:\Users\Administrator\Desktop\scripts\fas-folder -nex
                 ''')
-        parser.add_argument('-f',dest ='file',help='input fasta file',required=True)
+        parser.add_argument('-f',dest ='file',help='input fasta file or folder',required=True)
         parser.add_argument('-phy',dest ='phy',help='turn into phylip format',\
                             default=False,action='store_true')   
         parser.add_argument('-nex',dest ='nex',help='turn into nexus format',\
@@ -99,14 +101,36 @@ if __name__ == '__main__':
                             default=False,action='store_true') 
         parser.add_argument('-stat',dest ='stat',help='generate statistics of the fasta file',\
                             default=False,action='store_true')
+        parser.add_argument('-o',dest='out',help='folder of the out file',default=scripts_path+'/batch_out')
         myargs = parser.parse_args(sys.argv[1:])
         return myargs
     myargs = parameter()
-    def main():
-        myHandle = Handle_file(myargs.file)
+    def remove_dir(path):
+        filelist=os.listdir(path)  
+        for f in filelist:
+            filepath = os.path.join( path, f )  
+            if os.path.isfile(filepath):  
+                os.remove(filepath)  
+                print(filepath+" removed!")
+            elif os.path.isdir(filepath):  
+                shutil.rmtree(filepath,True)
+                print("dir "+filepath+" removed!")
+    try:
+        os.mkdir(myargs.out)
+    except FileExistsError:        
+        remove_dir(myargs.out)
+    def main(file):        
+        myHandle = Handle_file(file)
         myHandle.handle() 
         myHandle.judge()  
         myHandle.complete() 
-        myHandle.save(myargs.file) 
-    main()
+        myHandle.save(file) 
+    if os.path.isdir(myargs.file):
+        files = os.listdir(myargs.file)
+        for each_file in files:
+            main(myargs.file+'/'+each_file)
+    elif os.path.isfile(myargs.file): 
+        main(myargs.file)
+    else:
+        print('input error!')
     print('completed!')
